0648f1c3c10cadbfdda1bdc66c29c379e05f44c1
lab_services_student <lab_services_student@labVMH8OX>
2024-09-10 15:57:42 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace POE_PART1_PROGRAMMING_2B
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
